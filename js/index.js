0dccade1d02e5e0a382bd14cec05656dccea441c
jakejo <jakejo@kakao.com>
2024-05-28 09:30:39 +0900

    [REFACTOR] 버벅이는 현상 수정

--- js/index.js
+++ js/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function(){
       container: document.getElementById('graph_img'), // 필수, 애니메이션 들어가는 곳 
       path: './json/graph_img.json', // 필수(url 또는 json파일 다운로드 경로)
       renderer: 'svg', // 필수
-      loop: true, // 반복재생
+      loop: false, // 반복재생
       autoplay: true, // 자동재생
       name: "Hello World", // 이름 지정, 필수 아님
     });
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", function(){
         container: document.getElementById('loading_bar'), // 필수, 애니메이션 들어가는 곳 
         path: './json/loading_bar.json', // 필수(url 또는 json파일 다운로드 경로)
         renderer: 'svg', // 필수
-        loop: true, // 반복재생
+        loop: false, // 반복재생
         autoplay: true, // 자동재생
         name: "Hello World", // 이름 지정, 필수 아님
       });
@@ -19,7 +19,7 @@ document.addEventListener("DOMContentLoaded", function(){
         container: document.getElementById('open_run_festa'), // 필수, 애니메이션 들어가는 곳 
         path: './json/open_run_festa.json', // 필수(url 또는 json파일 다운로드 경로)
         renderer: 'svg', // 필수
-        loop: true, // 반복재생
+        loop: false, // 반복재생
         autoplay: true, // 자동재생
         name: "Hello World", // 이름 지정, 필수 아님
       });
